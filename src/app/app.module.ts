d95632f4323d5f3d559fa8c7bbf752c2a498693b
Elson Costa <elsongabriel@hotmail.com>
2019-04-29 15:41:58 -0300

    Usando navbar com bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {UserComponent} from './components/user/user.component';
 import {UsersComponent} from './components/users/users.component';
 
@@ -9,7 +10,8 @@ import {UsersComponent} from './components/users/users.component';
     declarations: [
         AppComponent,
         UserComponent,
-        UsersComponent
+        UsersComponent,
+        NavbarComponent
     ],
     imports: [
         BrowserModule
